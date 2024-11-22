cd34748ab1024d0fe07d154a94a96909fb68fbc8
Richard Watts <richard@zilliqa.com>
2024-11-22 18:21:13 +0000

    (fix) Rebuild

--- lib.esm/crypto/signature.js
+++ lib.esm/crypto/signature.js
@@ -40,7 +40,8 @@ export class Signature {
     set s(_value) {
         assertArgument(dataLength(_value) === 32, "invalid s", "value", _value);
         const value = hexlify(_value);
-        assertArgument(parseInt(value.substring(0, 3)) < 8, "non-canonical s", "value", value);
+        /* Zilliqa has a large number of non-canonical signatures */
+        /* assertArgument(parseInt(value.substring(0, 3)) < 8, "non-canonical s", "value", value); */
         this.#s = value;
     }
     /**
@@ -56,7 +57,8 @@ export class Signature {
     get v() { return this.#v; }
     set v(value) {
         const v = getNumber(value, "value");
-        assertArgument(v === 27 || v === 28, "invalid v", "v", value);
+        /* Zilliqa also has quite a lot of these, apparently */
+        /* assertArgument(v === 27 || v === 28, "invalid v", "v", value); */
         this.#v = v;
     }
     /**
@@ -207,7 +209,7 @@ export class Signature {
         if (bv === BN_1 || bv === BN_28) {
             return 28;
         }
-        assertArgument(bv >= BN_35, "invalid v", "v", v);
+        /* assertArgument(bv >= BN_35, "invalid v", "v", v); */
         // Otherwise, EIP-155 v means odd is 27 and even is 28
         return (bv & BN_1) ? 27 : 28;
     }
@@ -239,7 +241,7 @@ export class Signature {
             if (bytes.length === 65) {
                 const r = hexlify(bytes.slice(0, 32));
                 const s = bytes.slice(32, 64);
-                assertError((s[0] & 0x80) === 0, "non-canonical s");
+                /* assertError((s[0] & 0x80) === 0, "non-canonical s"); */
                 const v = Signature.getNormalizedV(bytes[64]);
                 return new Signature(_guard, r, hexlify(s), v);
             }
@@ -265,7 +267,7 @@ export class Signature {
             }
             assertError(false, "missing s");
         })(sig.s, sig.yParityAndS);
-        assertError((getBytes(s)[0] & 0x80) == 0, "non-canonical s");
+        /* assertError((getBytes(s)[0] & 0x80) == 0, "non-canonical s"); */
         // Get v; by any means necessary (we check consistency below)
         const { networkV, v } = (function (_v, yParityAndS, yParity) {
             if (_v != null) {
